ecc299e7a9fcfbad8e94dd3a6b60a43e44ad59de
SRIRAM <polamarasettisriramnaidu.16.cse@anits.edu.in>
2019-06-12 22:56:56 +0530

    created substract function

--- Math.js
+++ Math.js
@@ -1,7 +1,12 @@
 function add(a,b) {
     return a+b;
 }
+function substract(a,b){
+    return a-b;
+}
+
 
 module.exports={
-    add 
+    add,
+    substract
 }
--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 var Math=require('./Math');
 
+
 console.log(Math.add(10,20));
+console.log(Math.substract(50,10));
